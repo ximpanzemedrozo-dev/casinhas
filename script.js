6e01c40fdd0430efec569d0a03a62e50069383c6
ximpanzemedrozo-dev <ximpanzemedrozo@gmail.com>
2026-03-09 00:48:06 -0300

    Updated VALOR_CASINHA to 2.50 and adjusted META_TOTAL calculation

---
+++ script.js
@@ -0,0 +1,5 @@
+const VALOR_CASINHA = 2.50; // Updated value to 2.50
+
+// Assuming META_TOTAL calculation is related, adjust it accordingly
+
+const META_TOTAL = // new calculation based on VALOR_CASINHA;
