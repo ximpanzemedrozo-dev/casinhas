eb87f8ba3f222d61aacc14aa8c3a57c61ad2f12b
ximpanzemedrozo-dev <ximpanzemedrozo@gmail.com>
2026-03-11 02:09:20 -0300

    Delete dados-casinhas.js

--- dados-casinhas.js
+++
@@ -1,8 +0,0 @@
-// ===== DADOS DAS CASINHAS =====
-// Todas as mensagens foram movidas para game-animations.js
-
-function obterMensagem(numero) {
-    return { emoji: '🏠', texto: 'Casa ' + numero };
-}
-
-console.log('📝 dados-casinhas.js carregado');
